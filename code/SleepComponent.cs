fc975670ea4aa862b1fd40a3b0d404b204dde625
PolSpock <5229571+PolSpock@users.noreply.github.com>
2025-05-07 16:23:57 +0200

    Update SleepComponent.cs

--- code/SleepComponent.cs
+++ code/SleepComponent.cs
@@ -1,17 +1,9 @@
 using Sandbox;
 
-public sealed class SleepComponent : Component
+public class SleepComponent : Component
 {
 	[RequireComponent, Property] BoxCollider BoxCollider { get; set; }
 	[RequireComponent, Property] ModelRenderer ModelRenderer { get; set; }
-
-	protected override void OnStart()
-	{
-		base.OnStart();
-
-		BoxCollider.KeyframeBody.AutoSleep = true;
-		BoxCollider.KeyframeBody.Sleeping = true;
-	}
 
 	protected override void OnUpdate()
 	{
